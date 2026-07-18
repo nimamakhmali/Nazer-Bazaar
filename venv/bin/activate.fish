8458037b1ced6e81def5cfef397d79126a9add14
nimamakhmali <nmkhmly790@gmail.com>
2026-07-18 12:05:14 +0330

    update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/nimamakhmali/Documents/Rayan Pajouhan/Nazer-Bazaar/venv'
+set -gx VIRTUAL_ENV '/home/nimamakhmali/Documents/Rayan Pajouhan/Nazer-Bazaar/backend/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
